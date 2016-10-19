3ee4ce0de08934ff98acb5aa00abaf5363195cf5
mscampbell <mcampbell@ip-172-30-0-145.ec2.internal>
2016-10-19 01:42:09 +0000

    adding perl 1 solutions

---
+++ perl_1_problem_set.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "Hello New York";
+
+my $name = 'Rico Suave';
+
+print "$name";
+
+print "\n\n$name says:\n\tHello New York\n\n";
+
+print '\n\n$name says:\n\tHello New York\n\n';
+
+print "\n\n$name says:\n\tHello New York\nHigh 2+3\n\n";
+
+print '\n\n$name says:\n\tHello New York\nHigh 2+3\n\n';
+
+print "\n\n$name says:\n\tHello New York\nHigh", 2+3, "\n\n";
